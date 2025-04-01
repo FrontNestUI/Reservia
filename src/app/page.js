1ccf5761252e13adbe492d65a46c27f72078bf6d
FrontNestUI <frontnest.ui@gmail.com>
2025-04-01 17:22:33 +0200

    update folders

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
 import React, { lazy } from 'react';
-import Container from './components/Container';
-import Hero from "./components/Hero";
-import MainNavBar from "./components/MainNavBar";
-import NavCatg from "./components/NavCatg";
-import TagsContainer from "./components/TagsContainer";
-import SearchBar from "./components/SearchBar";
-import Footer from "./components/Footer";
-import DesktopNavBar from './components/DesktopNavBar';
-import ActiviteContainer from './components/ActiviteContainer';
+import Container from '../components/Container';
+import Hero from "../components/Hero";
+import MainNavBar from "../components/MainNavBar";
+import NavCatg from "../components/NavCatg";
+import TagsContainer from "../components/TagsContainer";
+import SearchBar from "../components/SearchBar";
+import Footer from "../components/Footer";
+import DesktopNavBar from '../components/DesktopNavBar';
+import ActiviteContainer from '../components/ActiviteContainer';
 //const Container = lazy(() => import('./components/Container'))
 
 export default function Home() {
